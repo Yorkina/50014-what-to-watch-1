d392a92107a05cce4f4aeeacfc63f3fd6df9af52
Natalia Yorkina <yorkinan@gmail.com>
2019-05-06 20:45:42 +0300

    add empty space

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
+
 const init = () => {
   const movieTitles = [
     `Fantastic Beasts: The Crimes of Grindelwald`,
